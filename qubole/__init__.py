2944a5136bfd8f808707e17fffbf4640181db42e
etheleon <picy2k@gmail.com>
2019-04-12 13:50:17 +0800

    Bump version: 0.1.6 → 0.1.7

--- qubole/__init__.py
+++ qubole/__init__.py
@@ -7,7 +7,7 @@ import pprint
 
 import requests
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 class Qubole:
     """Basic qubole wrapper"""
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
         name='pyqubole',
-        version='0.1.6',
+        version='0.1.7',
         author='Wesley Goi',
         author_email='picy2k@gmail.com',
         packages=['qubole'],
